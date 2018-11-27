ad91cffe570a0c982b1bc215b3a63d45ffd8f894
gihankarunarathne <gckarunarathne@gmail.com>
2018-11-27 13:17:54 +0400

    Implemented CRUD on extension->triggers

--- web/api/extension.py
+++ web/api/extension.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, json
 from sqlalchemy import text as sql
 
 from web import util
+from web.api import trigger
 
 bp = Blueprint('timeseries', __name__)
 ENGINE = util.get_engine('metadata')
@@ -25,16 +26,19 @@ ENGINE = util.get_engine('metadata')
     ],
     inputVariables: [],
     outputVariables: [],
-    trigger: {
-        type: enum("onChange", "onTime"),
-        on: []
-    },
+    trigger: [
+        {
+            trigger_type: enum("OnChange", "OnTime"),
+            trigger_on: []
+        }
+    ],
     options: {}
 }
 """
 @bp.route('/extension', methods=['POST'])
 def extension_create():
     data = request.get_json()
+    assert 'extensionId' in data, f'extensionId should be provided'
     assert 'variables' in data and isinstance(data['variables'], list), f'variables should be provided'
     data['variables'], variable_names = util.create_timeseries(data['variables'])
     if 'inputVariables' in data:
@@ -52,8 +56,10 @@ def extension_create():
         data['options'] = '{}'
     else:
         data['options'] = json.dumps(data['options'])
+    assert 'trigger' in data and isinstance(data['trigger'], list), f'trigger list should be provided'
 
     with ENGINE.begin() as conn:
+        trigger.extension_trigger_create(conn, data['extensionId'], data['trigger'])
         conn.execute(sql('''
             INSERT IGNORE INTO extensions (extensionId, extension, function, data, options)
             VALUES (:extensionId, :extension, :function, :data, :options)
@@ -73,6 +79,7 @@ def extension_get(extension_id):
     assert extension, f'Extension does not exists: {extension_id}'
     extension = dict(extension)
     data = json.loads(extension['data'])
+    data['trigger'] = trigger.extension_trigger_get(ENGINE, extension_id)
     extension['variables'] = data['variables']
     extension['inputVariables'] = data['inputVariables']
     extension['outputVariables'] = data['outputVariables']
@@ -83,6 +90,7 @@ def extension_get(extension_id):
 
 @bp.route("/extension/<extension_id>", methods=['DELETE'])
 def extension_delete(extension_id):
+    triggers = trigger.extension_trigger_delete(ENGINE, extension_id)
     extension = ENGINE.execute(sql('''
         DELETE FROM extensions
         WHERE extensionId=:extension_id
---
+++ web/api/trigger.py
@@ -0,0 +1,37 @@
+from sqlalchemy import text as sql
+
+
+def extension_trigger_create(conn, extension_id, triggers):
+    for t in triggers:
+        assert 'trigger_type' in t, f'trigger_type should be provided in each triggers'
+        assert t.get('trigger_type') in ['OnChange', 'OnTime'], \
+            f'trigger trigger_type should have one of "OnChange", "OnTime"'
+        assert 'trigger_on' in t and isinstance(t.get('trigger_on'), list), \
+            f'trigger_on as a list should be provided in each triggers'
+        for trigger_on in t.get('trigger_on', []):
+            conn.execute(sql('''
+                INSERT IGNORE INTO triggers (extensionId, `trigger_type`, `trigger_on`)
+                VALUES (:extension_id, :trigger_type, :trigger_on)
+            '''), extension_id=extension_id, trigger_type=t.get('trigger_type'), trigger_on=trigger_on)
+
+
+def extension_trigger_get(conn, extension_id):
+    rows = conn.execute(sql('''
+        SELECT trigger_type, GROUP_CONCAT(trigger_on) as trigger_on
+        FROM triggers WHERE extensionId=:extension_id GROUP BY trigger_type
+    '''), extension_id=extension_id).fetchall()
+    triggers = []
+    for r in rows:
+        triggers.append({
+            'trigger_type': r['trigger_type'],
+            'trigger_on': r['trigger_on'],
+        })
+    return triggers
+
+
+def extension_trigger_delete(conn, extension_id):
+    rows = conn.execute(sql('''
+        DELETE FROM triggers
+        WHERE extensionId=:extension_id
+    '''), extension_id=extension_id)
+    return rows
